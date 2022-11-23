2260268b4e2d6de631a1af13843ce93efe5353c0
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-11-23 20:57:36 +0900

    Create user and blog package and move files into the packages

--- 7_0/ch14/config/application.rb
+++ 7_0/ch14/config/application.rb
@@ -6,5 +6,7 @@ module SampleApp
   class Application < Rails::Application
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
+    config.paths.add 'packages/user', glob: '{app/*, app/*/concerns, lib}', eager_load: true
+    config.paths.add 'packages/blog', glob: '{app/*, app/*/concerns, lib}', eager_load: true
   end
 end
